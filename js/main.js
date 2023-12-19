11c979f0a210673432b362713b326a7a7d4d15f3
smjkr <smjakir2010@outlook.com>
2023-12-19 22:51:33 +0600

    added bank deposit and balance

---
+++ js/main.js
@@ -0,0 +1,45 @@
+//login button event handler
+const loginBtn=document.getElementById("login");
+loginBtn.addEventListener("click",function(){
+const loginArea=document.getElementById("login-area");
+loginArea.style.display = "none";
+const transactionArea = document.getElementById("transaction-area");
+transactionArea.style.display="block";
+})
+//deposit button event handler
+const depositBtn=document.getElementById("addDeposit");
+depositBtn.addEventListener('click',function(){
+  
+    const depositNumber = getInputNumber("depositAmount")
+
+    // const currentDeposit = document.getElementById('currentDeposit').innerText;
+    // const currentNuber = parseFloat (currentDeposit);
+    // const totalDeposit = depositNumber + currentNuber;
+    // document.getElementById('currentDeposit').innerText = totalDeposit;
+                
+    updateSpanText('currentDeposit', depositNumber);
+    updateSpanText('currentBalance', depositNumber);
+
+    document.getElementById("depositAmount").value="";
+});
+
+//withdraw button event handler
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener('click',function(){
+    
+})
+
+
+
+function getInputNumber(id) {
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
+
+function updateSpanText(id, depositNumber) {
+    const current = document.getElementById(id).innerText;
+    const currentNumber = parseFloat(current);
+    const total = depositNumber + currentNumber;
+    document.getElementById(id).innerText = total;
+}
